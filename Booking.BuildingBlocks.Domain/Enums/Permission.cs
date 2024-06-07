cf00d637ac7bc8d7a43658792b45351a9d11bf22
Krsmanovic Djordje (uif72067) <djordje1499@gmail.com>
2024-06-07 10:41:18 +0200

    feat: add permissions

--- Booking.BuildingBlocks.Domain/Enums/Permission.cs
+++ Booking.BuildingBlocks.Domain/Enums/Permission.cs
@@ -7,6 +7,12 @@
         GuestReservationOperations = 3,
         HostReservationOperations = 4,
         HostAccommodationOperations = 5,
-        ChangeUserInfo = 6
+        ChangeUserInfo = 6,
+        SubscriptionOperations = 7,
+        GuestReservationPayment = 8,
+        HostReservationPayment = 9,
+        ReservationOperations = 10,
+        InvoicesOperations = 11,
+        CreateReview = 12,
     }
 }
